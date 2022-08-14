57d9842b7f3ee181044a1b9fa4918812e20f5e4a
Patrick-Njiru <patrick.njiru@students.moringaschool.com>
2022-08-14 20:30:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee= {
+    name:  'Patrick Njiru',
+    streetAddress: 'Nairobi'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employee1={...employee} 
+    employee1[key] =value
+    return employee1
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key]=value
+   return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employee2={...employee}
+    delete employee2[key]
+    return employee2
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
